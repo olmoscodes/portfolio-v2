f3167379e1e469561d5138147da62442e68fdcb9
Marc Olmos Cabezas <marc@MacBook-Pro-de-Marc.local>
2021-08-27 17:51:13 +0200

    Background filter + home + presentation + projects component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PresentationComponent } from './home/presentation/presentation.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './home/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PresentationComponent,
+    HomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
