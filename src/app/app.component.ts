d44188723a53286de91d76e1b495ad56090b84f3
Marc Olmos Cabezas <marc@MacBook-Pro-de-Marc.local>
2021-08-28 11:46:05 +0200

    cursor animation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'portfolio-v2';
+
+
+
+  constructor() { }
+  
+
+  ngOnInit(): void {
+
+    let mouseCursor = document.getElementById('cursor')!;
+
+    window.addEventListener('mousemove', e => {
+      mouseCursor.style.top = e.pageY + 'px';
+      mouseCursor.style.left = e.pageX + 'px';
+      console.log(e.pageY);
+      console.log(e.pageX);
+    })
+  }
 }
